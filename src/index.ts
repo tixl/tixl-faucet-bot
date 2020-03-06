ee94662e799c679525f394af057970e925266852
Bernd Strehl <bernd.strehl@gmail.com>
2020-03-06 12:21:50 +0100

    add try catch

--- src/index.ts
+++ src/index.ts
@@ -21,49 +21,53 @@ bot.start((ctx: any) => {
   ctx.reply('Please enter your address.');
 });
 bot.on('text', async (ctx: any) => {
-  const address = ctx.message.text.trim();
-  const username = ctx.message.from.username;
-  log.info('User attempts to get funds', { address, username });
-  const canReceive = await canUserReceive(username);
+  try {
+    const address = ctx.message.text.trim();
+    const username = ctx.message.from.username;
+    log.info('User attempts to get funds', { address, username });
+    const canReceive = await canUserReceive(username);
 
-  if (isAdmin(username) || canReceive) {
-    if (isAddressValid(address)) {
-      log.info('User is being sent funds', { address, username });
-      try {
-        ctx.reply(`Understood. Your address is "${address}".`);
-        ctx.reply(`*Please stand by as I fulfill your request.*`, {
-          parse_mode: 'Markdown',
-        });
+    if (isAdmin(username) || canReceive) {
+      if (isAddressValid(address)) {
+        log.info('User is being sent funds', { address, username });
+        try {
+          ctx.reply(`Understood. Your address is "${address}".`);
+          ctx.reply(`*Please stand by as I fulfill your request.*`, {
+            parse_mode: 'Markdown',
+          });
 
-        const ntruPublicKey = await getNtruPublicKey(address);
+          const ntruPublicKey = await getNtruPublicKey(address);
 
-        if (!ntruPublicKey) {
-          log.info('Public signature key has no corresponding blockchain');
-          ctx.reply(`There does not seem to be a blockchain for this address.`);
-          return;
+          if (!ntruPublicKey) {
+            log.info('Public signature key has no corresponding blockchain');
+            ctx.reply(`There does not seem to be a blockchain for this address.`);
+            return;
+          }
+
+          const { sendAmount, hash } = await sendFromGenesis(ntruPublicKey);
+          const txlAmount = sendAmount / BigInt(Math.pow(10, 7));
+
+          ctx.reply(
+            `I just sent you ~${txlAmount} TXL, you should receive it soon. The hash of the transaction is ${hash}.`,
+          );
+          ctx.reply(`You can track the transaction on https://explorer.tixl.dev`);
+
+          log.info('User got confirmation', { address, username, amount: String(sendAmount), hash });
+          await updateOrCreateUserTimestamp(username);
+        } catch (error) {
+          ctx.reply(`Sorry, there was an error, please try again later.`);
+          log.error(error);
         }
-
-        const { sendAmount, hash } = await sendFromGenesis(ntruPublicKey);
-        const txlAmount = sendAmount / BigInt(Math.pow(10, 7));
-
-        ctx.reply(
-          `I just sent you ~${txlAmount} TXL, you should receive it soon. The hash of the transaction is ${hash}.`,
-        );
-        ctx.reply(`You can track the transaction on https://explorer.tixl.dev`);
-
-        log.info('User got confirmation', { address, username, amount: String(sendAmount), hash });
-        await updateOrCreateUserTimestamp(username);
-      } catch (error) {
-        ctx.reply(`Sorry, there was an error, please try again later.`);
-        log.error(error);
+      } else {
+        log.info('User entered invalid address', { address, username });
+        ctx.reply('This does not seem like a valid address. Check your address and send it again.');
       }
     } else {
-      log.info('User entered invalid address', { address, username });
-      ctx.reply('This does not seem like a valid address. Check your address and send it again.');
+      log.info('User is on timeout', { address, username });
+      ctx.reply(`You aleady received tokens for this account. Please wait 60 minutes.`);
     }
-  } else {
-    log.info('User is on timeout', { address, username });
-    ctx.reply(`You aleady received tokens for this account. Please wait 60 minutes.`);
+  } catch (error) {
+    log.error(error);
   }
 });
 bot.launch();
